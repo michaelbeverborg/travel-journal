4cb9b78b2f6832a1c704fb34da7e2ba2e5d165f9
Michael <michaelbeverborg@hotmail.com>
2022-02-28 15:24:53 +0100

    Final

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,19 @@ const Card = props => {
   return (
     <div className="card">
       <img src={props.imageUrl} alt="aaaa" />
-      <p className="card-title">{props.title}</p>
+      <div className="card-text">
+        <h3 className="card-title">
+          {props.title} in {props.location}
+        </h3>
+        <a target="_blank" href={props.googleMapsUrl}>
+          Google Maps
+        </a>
+        <br />
+        <small>
+          {props.startDate} - {props.endDate}
+        </small>
+        <p>{props.description}</p>
+      </div>
     </div>
   );
 };
